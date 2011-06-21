d6476a86c156aae35e6ec114f2d1a0a68694019d
ggoodman <ggoodman@gmail.com>
2011-06-21 18:34:43 +0000

    Memory optimization for maps objects

--- public/lib/mtlbixi.js
+++ public/lib/mtlbixi.js
@@ -1,14 +1,13 @@
 $(function(){
   var options = {
     zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    streetViewControl: false
   };
   
   var map = new google.maps.Map(document.getElementById("map_canvas"), options);
   var markers = {};
   var location = null;
-  var idleQueue = new Queue()
-    , ajaxQueue = new Queue();
 
   //var bikeLayer = new google.maps.BicyclingLayer();
   //bikeLayer.setMap(map);
@@ -61,31 +60,43 @@ $(function(){
         dataType: "json",
         error: deferred.reject,
         success: function(data) {
+          var markerSize = new google.maps.Size(30,23)
+            , markerTopLeft = new google.maps.Point(0,0)
+            , markerBottomRight = new google.maps.Point(15,23);
+          var markerMixed = new google.maps.MarkerImage(
+            '/img/marker-mixed.png',
+            markerSize,
+            markerTopLeft,
+            markerBottomRight
+          );
+          var markerEmpty = new google.maps.MarkerImage(
+            '/img/marker-empty.png',
+            markerSize,
+            markerTopLeft,
+            markerBottomRight
+          );
+          var markerFull = new google.maps.MarkerImage(
+            '/img/marker-full.png',
+            markerSize,
+            markerTopLeft,
+            markerBottomRight
+          );
+          var markerShadow = new google.maps.MarkerImage(
+            '/img/marker-shadow.png',
+            new google.maps.Size(46,23),
+            markerTopLeft,
+            markerBottomRight
+          );
+          var markerShape = {
+            coord: [19,0,21,1,22,2,22,3,23,4,23,5,24,6,24,7,24,8,24,9,24,10,24,11,24,12,23,13,23,14,22,15,21,16,20,17,19,18,18,19,17,20,16,21,15,22,15,22,14,21,13,20,12,19,11,18,9,17,8,16,7,15,7,14,6,13,6,12,6,11,6,10,6,9,6,8,6,7,6,6,6,5,7,4,7,3,8,2,9,1,11,0,19,0],
+            type: 'poly'
+          };            
           jQuery.each(data, function(i, station) {
-            var status = station.bikes ? (station.free ? 'mixed' : 'empty') : 'full';
-            
-            var image = new google.maps.MarkerImage(
-              '/img/marker-' + status  + '.png',
-              new google.maps.Size(30,23),
-              new google.maps.Point(0,0),
-              new google.maps.Point(15,23)
-            );
-            
-            var shadow = new google.maps.MarkerImage(
-              '/img/marker-shadow.png',
-              new google.maps.Size(46,23),
-              new google.maps.Point(0,0),
-              new google.maps.Point(15,23)
-            );
-            
-            var shape = {
-              coord: [19,0,21,1,22,2,22,3,23,4,23,5,24,6,24,7,24,8,24,9,24,10,24,11,24,12,23,13,23,14,22,15,21,16,20,17,19,18,18,19,17,20,16,21,15,22,15,22,14,21,13,20,12,19,11,18,9,17,8,16,7,15,7,14,6,13,6,12,6,11,6,10,6,9,6,8,6,7,6,6,6,5,7,4,7,3,8,2,9,1,11,0,19,0],
-              type: 'poly'
-            };
+
             var marker = new google.maps.Marker({
-              icon: image,
-              shadow: shadow,
-              shape: shape,
+              icon: station.bikes ? (station.free ? markerMixed : markerEmpty) : markerFull,
+              shadow: markerShadow,
+              shape: markerShape,
               position: new google.maps.LatLng(station.loc.lat, station.loc.lng), 
               map: null, 
               title: station.name
